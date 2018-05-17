6ef8e6c7ca59f291fbe1832606061cf96ca7ea12
luznykarol <karol.rudzinski1990@gmail.com>
2018-05-18 00:38:20 +0200

    3rd commit
    +placeholder footer/contact section.
    +correct links to projects
    +smooth scroll
    +cleared some naming mistakes

--- js/script.js
+++ js/script.js
@@ -11,5 +11,14 @@ $(document).ready(function() {
 	$('.navigation__link').click(function () {
 		$('.navigation__list').slideToggle();
 		$('.burger').toggleClass('open');
-    });
+	});
+	
+	var navbarHeight = $('.navigation__nav').outerHeight();
+
+	$('a').click(function(){
+		$('html, body').animate({
+			scrollTop: $( $(this).attr('href') ).offset().top -navbarHeight
+		}, 1000);
+		return false;
+	});
 });
